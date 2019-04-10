57101d258b631e849b6730dec3300f8574307633
Meenu Mongia <meenu.mongia@gmail.com>
2019-04-10 16:31:07 +0530

    data object added for fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,81 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
-  title = 'assign';
-  public calculateDiv: Boolean = false;
-  public displayDialog: Boolean = false;
-  public countryData = [
-      {
-        'name': 'Croatia',
-        'id': 'cr'
-      },
-      {
-        'name': 'France',
-        'id': 'fr'
-      },
-      {
-        'name': 'Germany',
-        'id': 'gr'
-      },
-      {
-        'name': 'Italy',
-        'id': 'it'
-      },
-      {
-        'name': 'Spain',
-        'id': 'sp'
-      },
-      {
-        'name': 'United Kingdom',
-        'id': 'uk'
-      }
-  ];
-  public repaymenttype = [
-    {
-      'name': 'xyz',
-      'id': 'x'
-    },
-    {
-      'name': 'abc',
-      'id': 'a'
-    }
+	title = 'assign';
+	public calculateDiv: Boolean = false;
+	public displayDialog: Boolean = false;
+	public countryData = [
+			{
+				'name': 'Croatia',
+				'id': 'cr'
+			},
+			{
+				'name': 'France',
+				'id': 'fr'
+			},
+			{
+				'name': 'Germany',
+				'id': 'gr'
+			},
+			{
+				'name': 'Italy',
+				'id': 'it'
+			},
+			{
+				'name': 'Spain',
+				'id': 'sp'
+			},
+			{
+				'name': 'United Kingdom',
+				'id': 'uk'
+			}
+	];
+	public repaymenttype = [
+		{
+			'name': 'xyz',
+			'id': 'x'
+		},
+		{
+			'name': 'abc',
+			'id': 'a'
+		}
 ];
+	public clientDetails : any;
 constructor(
 ) {
+		this.clientDetails = {
+			name: 'test user',
+			productType: 2,
+			limitSize: 10,
+			repaymentType : 1,
+			utilization : 100,
+			forecastUtilization : 1
+		}
 }
 ngOnInit() {
 }
 newDiv() {
-  this.calculateDiv = true;
+	this.calculateDiv = true;
 }
 popUp() {
-  this.displayDialog = true;
+	this.displayDialog = true;
+}
+
+changeData() {
+console.log("  change data called");
+	this.clientDetails = {
+			name: 'test user 11',
+			productType: 2,
+			limitSize: 10000,
+			repaymentType : 1,
+			utilization : 500,
+			forecastUtilization : 1
+		}
 }
 
 
